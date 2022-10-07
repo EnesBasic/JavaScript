8cdc156d282e88c618de1b33d7e936f09790483c
Enes Basic <ssamss3004@gmail.com>
2022-10-07 17:36:24 +0200

    init

--- JS_2/zad1_ver1/script.js
+++ JS_2/zad1_ver1/script.js
@@ -2,17 +2,17 @@
 function dodajuKorpu(){
 
     let mainEL = element.closest('.sadrzaj');
-    let cijena = mainEL.querySelector('cijena').innerText;
+    let cijena = mainEL.querySelector('.cijena').innerText;
     let ime = mainEL.querySelector('h3').innerText;
     let kolicina = mainEL.querySelector('input').value;
-    let korpaFilmovi = document.querySelector('.korpaFilmovi');
+    let korpaFilmovi = document.querySelector('.korpa-filmovi');
 
     if(parseInt(kolicina)>0){
         
         cijena = cijena.substring(1);
         console.log(typeof(cijena));
-        console.log(ime);
-        console.log(korpaFilmovi);
+        console.log(typeof(kolicina));
+
 
     }
 
