79bc8b6b491f64fab8ef3efcc47e8c6dbbe89597
Enes Basic <ssamss3004@gmail.com>
2022-10-11 12:53:41 +0200

    init

--- JS_2/zad1_ver1/script.js
+++ JS_2/zad1_ver1/script.js
@@ -1,5 +1,4 @@
-
-
+alltotal = 0;
 
 function dodajuKorpu(element){
 
@@ -14,11 +13,12 @@ function dodajuKorpu(element){
         cijena = cijena.substring(1);
         console.log(typeof(cijena));
         console.log(typeof(kolicina));
-        cijena = parseInt(cijena);
+        cijena = parseInt(cijena);0
         kolicina = parseInt(kolicina);
 
-        let total = cijena * kolicina;
-        
+        let total = cijena * kolicina;0
+        alltotal += total;
+
         korpaFilmovi.innerHTML +=
         `<div class= korpa-film>
         <h3>${name}</h3>
@@ -27,6 +27,33 @@ function dodajuKorpu(element){
 
         document.querySelector('.total').innerText = `total: ${alltotal} €`;
 
+        element.innerText = 'Dodato';
+        element.setAttribute('disabled', true);
     }
-
+    else{
+        console.log('Odaberi kolicinu');
+    }
 };
+
+function ukloniizKorpe(element){
+    let mainEl = element.closest('.korpa-filmovi');
+    let cijena = mainEl.querySelector('p span').innerText;
+    let name = mainEl.querySelector('h3').innerText;
+    let filmovi = document.querySelectorAll('.film');
+
+    console.log(cijena);
+    cijena = parseInt(cijena);
+    alltotal -= cijena;
+    document.querySelector('.total').innerText = `Total: ${alltotal} €`;
+    mainEl.remove();
+    console.log(filmovi);
+
+    filmovi.forEach(function(filmici){
+        let itemName = filmici.querySelector('.sadrzaj h3').innerText;
+        if (itemName === name){
+            filmici.querySelector('.akcija input').value = 0;
+            filmici.querySelector('.akcija button').removeAttribute('disabled');
+            filmici.querySelector('.akcija button').innerText = 'Dodaj';
+        }
+    });
+}
