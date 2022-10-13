c06931eaeb70fed65c968e8f472164690781416e
Enes Basic <ssamss3004@gmail.com>
2022-10-13 18:53:20 +0200

    init

--- JS_3/kopija/script.js
+++ JS_3/kopija/script.js
@@ -1,4 +1,4 @@
-/*----TRADICIONALNI nacin pravljenja funkcija u JS-u-----
+/*----TRADICIONALNI nacin pravljenja funkcija u JS-u---*/
 
 function klikni_me(){
     let a = 5;
@@ -8,7 +8,7 @@ function klikni_me(){
 
 klikni_me();
 
-/*----NOVI Nacin---------------------------------------*/
+/*----NOVI Nacin---se zove ARROW FUNCTION--------------*/
 
 const klikni_me_nova = () => {
     let a = 15;
@@ -17,6 +17,32 @@ const klikni_me_nova = () => {
 }
 
 klikni_me_nova();
+
+/*----Treci nacin -------------------------------------*/
+
+function klikni_me1( a , b ){// ako imamo funkciju koja ima liniju  
+    //let a = 5;             // mozemo bez viticastih zagrada
+    //let b = 10;
+    console.log( a + b );
+}
+
+klikni_me1(80,15);
+
+/*----Cetvrti nacin -----------------------------------*/
+const klikni_me_nova1 = b => { // ako imamo jedan argument
+    let a = 10;                // mozemo bey zagrada
+  //let b = 20;
+    console.log( a + b );
+}
+
+klikni_me_nova1(20);
+
+
+
+
+
+
+
 
 
 
