9b57710dc40776b3266500e8d4c4ee15158d5295
Enes Basic <ssamss3004@gmail.com>
2022-10-13 20:13:25 +0200

    init

--- JS_3/kopija/eventi.js
+++ JS_3/kopija/eventi.js
@@ -6,7 +6,7 @@
 5.) submit
 6.) change
 7.) resize
-----------------------------------------------------------------*/
+----------------------------------------------------------------
 
 const klikni_me = (dugme) => {
     console.log(dugme.innerText);
@@ -23,6 +23,57 @@ btns.forEach(function(btn) {
         let clicked_btn = event.target;
         console.log(clicked_btn.innerText);
     });
-    
+});
+
+let link = document.querySelector('#link');
+link.addEventListener('click',(event) => {
+    event.preventDefault();// ako hocemo da sprijecimo defaultno ponasanje
+    console.log(event.target);
+    });
+
+/*-----konkretan primjer -----------------------------------------------*/
+
+    let form = document.querySelector('form');
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log('test');
+    });
+
+/*-----select i resize -------------------------------------------------*/
+
+let opcije = document.querySelector('select');
+opcije.addEventListener("change", (event) => {
+    console.log(event.target.value);
+});
+
+window.addEventListener("resize", () => {
+    //console.log(window.innerWidth);
+    if(innerWidth > 300){
+        console.log('prozor veci od 500')
+    }else{
+        console.log('prozor manji od 500');
+    }
+});
+
+
+let input = document.querySelector('input');
+input.addEventListener("keydown", (event)=> {
+    if(event.key === 'Delete'){        
+        alert('Obrisao si nesto');
+    }
+});
+
+/*-----detektovanje pomjeranje misa unutar cijelog prozora-------------------*/
+
+window.addEventListener("mousemove", (event)=>{
+    console.log('mis pomjeren');
+});
+
+/*-----detektovanje pomjeranje misa unutar samo input polja------------------*/
+
+
+input.addEventListener("mousemove", (event)=> {
+    console.log('mis pomjeren');
 
 });
