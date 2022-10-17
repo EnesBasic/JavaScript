b22d0ce96ae1b117948371fa71498824e75bbf40
Enes Basic <ssamss3004@gmail.com>
2022-10-17 10:14:45 +0200

    init

--- JS_3/projMaster3/script.js
+++ JS_3/projMaster3/script.js
@@ -1,7 +1,10 @@
 window.addEventListener("keydown", (event) => {
-     let code = event.keycode;
-     
-     let audio = document.querySelector(`audio[data-key="${code}"]`);
-     audio.currentTime = 0;
-     audio.play();
+    let code = event.keyCode;
+    let keyElement = document.querySelector(`div[data-key="${code}"]`);
+   
+    if (!keyElement) return;
+    
+    let audio = document.querySelector(`audio[data-key="${code}"]`);
+    audio.currentTime = 0;
+    audio.play();
 });
